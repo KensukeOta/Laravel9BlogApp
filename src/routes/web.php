0773b0bca43b80d52f57241da81c9581f2370315
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-03-02 00:11:32 +0900

    記事編集ページへ遷移するためのルーティングを実装 #5

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,10 +16,10 @@ use App\Http\Controllers\PostController;
 |
 */
 
-
 Route::middleware('auth')->group(function () {
     Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
     Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+    Route::get('posts/{id}/edit', [PostController::class, 'edit'])->name('posts.edit');
 });
 
 Route::middleware('guest')->group(function () {
