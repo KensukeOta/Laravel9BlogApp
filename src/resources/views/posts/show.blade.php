8eafe978691cefb1d1f9dc980174d245fac69b8f
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-03-01 16:37:14 +0900

    修正

--- src/resources/views/posts/show.blade.php
+++ src/resources/views/posts/show.blade.php
@@ -3,6 +3,6 @@
         {{ $post->title }} - Laravel9 BlogApp
     </x-slot>
 
-    <h1 class="font-bold">{{ $post->title }}</h1>
+    <h1>{{ $post->title }}</h1>
     <p>{{ $post->body }}</p>
 </x-layout>
